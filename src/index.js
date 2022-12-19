acd52fa1cb630f9780bfe9f2866e06d99cf501a8
jorgegoco <jorgegoco70@gmail.com>
2022-12-19 19:48:25 +0100

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/configureStore';
-import App from './components/App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
