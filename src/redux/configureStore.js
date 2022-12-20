eb20cc00c8877844e9acc07f11bff9e41944bee5
jorgegoco <jorgegoco70@gmail.com>
2022-12-20 13:11:03 +0100

    Add books

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import reducerBook from './books/books';
 import reducerCategories from './categories/categories';
 
-const store = configureStore({
+const reducer = combineReducers({
   books: reducerBook,
   categories: reducerCategories,
+});
+
+const store = configureStore({
+  reducer,
 });
 
 export default store;
