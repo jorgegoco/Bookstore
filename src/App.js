6f6f3c6d4b5a4338599483807665760a656ec49a
jorgegoco <jorgegoco70@gmail.com>
2022-12-21 12:43:19 +0100

    Fix more linter errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes, Link } from 'react-router-dom';
-import Books from './components/Books.js';
-import Categories from './components/Categories.js';
+import Books from './Components/Books';
+import Categories from './Components/Categories';
 
 const App = () => (
   <>
