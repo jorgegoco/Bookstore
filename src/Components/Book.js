7e2a02ff6d507d04ee92a0f5c0d99b78ea5c7f92
jorgegoco <jorgegoco70@gmail.com>
2022-12-21 19:54:13 +0100

    Refactor with API

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
+import { removeBookAsync } from '../redux/books/books';
 
 const Book = ({ book }) => {
   const dispatch = useDispatch();
@@ -9,7 +9,7 @@ const Book = ({ book }) => {
     <section className="bookSection">
       <h2>{book.title}</h2>
       <p>{book.author}</p>
-      <button type="button" onClick={() => { dispatch(removeBook(book.id)); }}>
+      <button type="button" onClick={() => { dispatch(removeBookAsync(book.item_id)); }}>
         Remove Book
       </button>
     </section>
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,41 +1,84 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
+const LIST = 'bookstore/books/LIST';
 
-const books = [
-  {
-    id: 1,
-    title: 'The Shining',
-    author: 'Stephen King',
-  },
-  {
-    id: 2,
-    title: 'Dune',
-    author: 'Frank Herbert',
-  },
-  {
-    id: 3,
-    title: 'A Brief Story of Time',
-    author: 'Stephen Hawking',
-  },
-];
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/QgtuyWt2y7sUT1m9eRR5/books';
 
-export const addBook = (payload) => ({
-  type: ADD,
-  payload,
-});
+// const books = [
+//   {
+//     id: 1,
+//     title: 'The Shining',
+//     author: 'Stephen King',
+//   },
+//   {
+//     id: 2,
+//     title: 'Dune',
+//     author: 'Frank Herbert',
+//   },
+//   {
+//     id: 3,
+//     title: 'A Brief Story of Time',
+//     author: 'Stephen Hawking',
+//   },
+// ];
 
-export const removeBook = (payload) => ({
-  type: REMOVE,
-  payload,
-});
+const getAllBooks = async () => {
+  const result = await fetch(URL);
+  return result.json();
+};
 
-const reducerBooks = (state = books, action) => {
+export const getAllBooksAsync = createAsyncThunk(
+  LIST,
+  async () => getAllBooks().then((books) => Object.entries(books)
+    .map(([id, book]) => ({ item_id: id, ...book[0] }))),
+);
+
+const addBook = async (book) => {
+  const result = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(book),
+  });
+
+  return result;
+};
+
+export const addBookAsync = createAsyncThunk(
+  ADD,
+  async (book) => addBook(book).then(() => book),
+);
+
+const deleteBook = async (id) => {
+  const result = await fetch(`${URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: id,
+    }),
+  });
+  return result;
+};
+
+export const removeBookAsync = createAsyncThunk(
+  REMOVE,
+  async (id) => deleteBook(id).then(() => id),
+);
+
+const reducerBooks = (state = [], action) => {
   switch (action.type) {
-    case ADD:
+    case `${LIST}/fulfilled`:
       return [
         ...state, action.payload];
-    case REMOVE:
-      return state.filter((item) => item.id !== action.payload);
+    case `${ADD}/fulfilled`:
+      return [...state, action.payload];
+    case `${REMOVE}/fulfilled`:
+      return state.filter((book) => book.item_id !== action.payload);
     default:
       return state;
   }
